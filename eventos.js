8600afe4d0dcb00d5a1db095b7315ed45ec1ac70
nico-bondarenco <140919682+nico-bondarenco@users.noreply.github.com>
2024-03-25 13:31:04 -0300

    mas lindo y redondeado

--- eventos.js
+++ eventos.js
@@ -5,13 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
         let ta = document.getElementById("ta").value
         let nh = document.getElementById("nh").value
         let ma = document.getElementById("ma").value
-
-        document.getElementById("resultado").innerText = ((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1)  
+        let num = ((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1)  
+        document.getElementById("resultado").innerText =(Math.round(num * 100) / 100).toFixed(2); 
     }
 
     document.getElementById("enviador").addEventListener("click", calcular)
-
-    console.log(((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1))
 })
 
 
