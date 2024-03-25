9e8aa18873c1fb31e45cf9b207dd356958b946cf
nico-bondarenco <140919682+nico-bondarenco@users.noreply.github.com>
2024-03-25 13:35:05 -0300

    Que no baje de 0 grados

--- eventos.js
+++ eventos.js
@@ -5,7 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
         let ta = document.getElementById("ta").value
         let nh = document.getElementById("nh").value
         let ma = document.getElementById("ma").value
-        let num = ((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1)  
+        let num = ((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1)
+
+        if (num < 0) {
+            document.getElementById("resultado").innerText = 0
+        } else {
+            document.getElementById("resultado").innerText =(Math.round(num * 100) / 100).toFixed(2);
+        }
         document.getElementById("resultado").innerText =(Math.round(num * 100) / 100).toFixed(2); 
     }
 
