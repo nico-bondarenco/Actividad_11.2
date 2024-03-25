7f3744ca922d582cce9f27c530c901a2d60e64b9
nico-bondarenco <140919682+nico-bondarenco@users.noreply.github.com>
2024-03-25 16:16:57 -0300

    ahora?

--- eventos.js
+++ eventos.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let ta = document.getElementById("ta").value
         let nh = document.getElementById("nh").value
         let ma = document.getElementById("ma").value
-        let num = ((ma*ta - (88.83*10*nh)) / (ma + (10*nh)))
+        let num = ((ma*ta - (890*nh)) / (ma + (10*nh)))
 
         if (num < 0) {
             document.getElementById("resultado").innerText = 0
