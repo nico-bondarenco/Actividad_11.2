2391edb6c772cd22dd0b650b1d55b87d4ff3ae8f
nico-bondarenco <140919682+nico-bondarenco@users.noreply.github.com>
2024-03-25 13:20:33 -0300

    habia puesto h en vez de nh

--- eventos.js
+++ eventos.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let nh = document.getElementById("nh").value
         let ma = document.getElementById("ma").value
 
-        document.getElementById("resultado").value = ((ta*ma/(10*h))-70.83)/((ma/(10*nh))+1)  
+        document.getElementById("resultado").value = ((ta*ma/(10*nh))-70.83)/((ma/(10*nh))+1)  
     }
 
     document.getElementById("enviador").addEventListener("click", calcular)
